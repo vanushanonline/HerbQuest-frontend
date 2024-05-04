5ca96b42de3bb4f5e256a454b67150d64c9d6de9
AnushanOnline <vanushanonline@gmail.com>
2024-05-04 23:48:15 +0530

    Added BaseURL of the Render

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-
-const baseURL = 'http://127.0.0.1:8000/'
+const baseURL = 'https://herbquest.onrender.com'
+//const baseURL = 'http://127.0.0.1:8000/'
 const API = axios.create({ baseURL });
 
 API.interceptors.request.use(
