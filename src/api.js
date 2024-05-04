19170a59e7805a694f755b2a7cd35ebb146c807b
AnushanOnline <vanushanonline@gmail.com>
2024-05-05 01:16:13 +0530

    Updated api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'https://herbquest.onrender.com'
+const baseURL = 'https://herbquest.onrender.com/'
 //const baseURL = 'http://127.0.0.1:8000/'
 const API = axios.create({ baseURL });
 
