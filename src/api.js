ef4b8d27cce7583c502d52474e646677eab082c8
AnushanOnline <vanushanonline@gmail.com>
2024-09-14 23:22:31 +0530

    updated base url

--- src/api.js
+++ src/api.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 //const baseURL = 'https://herbquest.onrender.com/'
-const baseURL = 'http://127.0.0.1:8000/'
+const baseURL = 'https://a7f6-2402-4000-2181-9af2-9c2-8976-7ad4-a04e.ngrok-free.app/'
 const API = axios.create({ baseURL });
 
 API.interceptors.request.use(
     async config => {
         const newToken = await refreshToken();
         config.headers.Authorization = `${newToken.token_type} ${newToken.access_token}`;
+        config.headers["ngrok-skip-browser-warning"] = true
         config.withCredentials = true;  
         return config;
     },
